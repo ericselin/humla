c3a2d5bedab75acd0f5c598771b5e0019b15d477
Eric Selin <eric.selin@gmail.com>
2019-05-15 19:18:24 +0200

    Made completed poc and login

--- wc/ha-list.js
+++ wc/ha-list.js
@@ -1,5 +1,14 @@
 import { waitForAuth, Todos } from './lib/firebase.js';
 
+/**
+ * @param {MouseEvent} event
+ */
+const completedClick = (event) => {
+  const todo = /** @type {HTMLElement} */ (event.target).closest('ha-todo');
+  if (todo.hasAttribute('completed')) todo.removeAttribute('completed');
+  else todo.setAttribute('completed', '');
+};
+
 window.customElements.define(
   'ha-list',
   class extends HTMLElement {
@@ -12,9 +21,13 @@ window.customElements.define(
       this.innerHTML = '';
 
       todos.forEach((todo) => {
-        const el = /** @type {DocumentFragment} */ (template.content.cloneNode(true));
-        el.querySelector('ha-title').innerHTML = todo.title;
-        this.appendChild(el);
+        const doc = /** @type {DocumentFragment} */ (template.content.cloneNode(true));
+        const element = /** @type {HTMLElement} */ (doc.querySelector('ha-todo'));
+        element.dataset.id = todo.id;
+        if (todo.completed) element.setAttribute('completed', '');
+        doc.querySelector('ha-title').innerHTML = todo.title;
+        doc.querySelector('button').addEventListener('click', completedClick);
+        this.appendChild(doc);
       });
 
       if (todos.length === 0) console.log('No todos found.');
--- wc/lib/firebase.js
+++ wc/lib/firebase.js
@@ -9,7 +9,13 @@ export const waitForAuth = () => new Promise((resolve, reject) => {
     if (user) {
       resolve(`${user.email} signed in`);
     } else {
-      reject(new Error('User signed out'));
+      reject(new Error('User signed out, trying to sign in now'));
+      const authProvider = new firebase.auth.GoogleAuthProvider();
+      if (window.innerWidth <= 768) {
+        firebase.auth().signInWithRedirect(authProvider);
+      } else {
+        firebase.auth().signInWithPopup(authProvider);
+      }
     }
   });
 });
@@ -36,7 +42,7 @@ export class Todos {
 
   async get() {
     const snapshot = await this.todos.get();
-    const data = snapshot.docs.map(doc => doc.data());
+    const data = snapshot.docs.map(doc => Object.assign({ id: doc.id }, doc.data()));
     return /** @type {Todo[]} */ (data);
   }
 }
@@ -46,8 +52,9 @@ export default firebase;
 /**
  * @typedef Todo
  * @property {string} title
- * @property {boolean} completed
+ * @property {string} completed
  * @property {string} context
  * @property {string[]} tags
  * @property {string} soft
+ * @property {string} id
  */
